afac4a3fdd6db24bc82b580ba5c82f3644abba3c
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-02 12:06:37 +0000

    Jun 2, 2024, 7:06 PM

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>My website</h1>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
