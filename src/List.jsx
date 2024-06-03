6f0ead867fa3043d9780d39e2a797833ab1e6c95
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 13:28:22 +0000

    Jun 3, 2024, 8:28 PM

--- src/List.jsx
+++ src/List.jsx
@@ -6,8 +6,18 @@ function List() {
     { id: 4, name: 'coconut', calories: 159 },
     { id: 5, name: 'pineapple', calories: 37 },
   ]
+  //   fruits.sort((a, b) => a.name.localeCompare(b.name)) // ALPHABETICAL
+  //   fruits.sort((a, b) => b.name.localeCompare(a.name)) // REVERSE ALPHABETICAL
+  //fruits.sort((a, b) => a.calories - b.calories) // NUMERIC
+  //fruits.sort((a, b) => b.calories - a.calories) // REVERSE NUMERIC
 
-  const listItems = fruits.map((fruit) => <li key={fruit.id}>{fruit.name}</li>)
+  const lowCalFruits = fruits.filter((fruit) => fruit.calories < 100)
+
+  const listItems = lowCalFruits.map((lowCalFruit) => (
+    <li key={lowCalFruit.id}>
+      {lowCalFruit.name}: &nbsp; <b>{lowCalFruit.calories}</b>
+    </li>
+  ))
   return <ol>{listItems}</ol>
 }
 export default List
