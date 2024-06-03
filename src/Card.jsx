d075a36cd891b41fac218d9fc910b7b669fa12ee
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 09:17:57 +0700

    Learning react

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,8 +3,8 @@ function Card() {
   return (
     <div className="card">
       <img className="card-image" src={profilePic} alt="profile picture"></img>
-      <h2>Nicholas</h2>
-      <p>I make Youtube videos and play video games</p>
+      <h2 className="card-title">Nicholas</h2>
+      <p className="card-text">I make Youtube videos and play video games</p>
     </div>
   )
 }
