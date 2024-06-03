c4950563997fcf5f83512943618990481201e222
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 13:00:23 +0000

    Jun 3, 2024, 8:00 PM

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import UserGreeting from './UserGreeting.jsx'
 function App() {
   return (
     <>
-      <UserGreeting isLoggedIn={true} username="BroCode" />
+      <UserGreeting isLoggedIn={false} username="NirTinction" />
     </>
   )
 }
