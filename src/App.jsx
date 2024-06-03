45a5899fa7d11a66201168e0968ebf290ed09d9a
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 13:23:28 +0000

    Jun 3, 2024, 8:23 PM

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Card from './Card.jsx'
 import Button from './Button.jsx'
 import Student from './Student.jsx'
 import UserGreeting from './UserGreeting.jsx'
-
+import List from './List.jsx'
 function App() {
   return (
     <>
-      <UserGreeting isLoggedIn={false} username="Nicho" />
+      <List />
     </>
   )
 }
