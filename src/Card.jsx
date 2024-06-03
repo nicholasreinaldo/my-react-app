1bceebd2718bc547b1384b3a3db95bef87408245
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 02:03:31 +0000

    Jun 3, 2024, 9:03 AM

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,10 @@
 function Card() {
   return (
     <div className="card">
-      <img></img>
+      <img
+        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg"
+        alt="profile picture"
+      ></img>
       <h2>Nicholas</h2>
       <p>I make Youtube videos and play video games</p>
     </div>
