5a900a0a009ea24d3b00c7b360dfff62a474b591
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 02:13:57 +0000

    Jun 3, 2024, 9:13 AM

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import profilePic from './assets/profile.jpg'
 function Card() {
   return (
     <div className="card">
-      <img src={profilePic} alt="profile picture"></img>
+      <img className="card-image" src={profilePic} alt="profile picture"></img>
       <h2>Nicholas</h2>
       <p>I make Youtube videos and play video games</p>
     </div>
