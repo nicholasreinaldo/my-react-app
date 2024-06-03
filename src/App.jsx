96c19b20c4a0fd8f9f83b5560329cd0ae8a75221
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 13:32:58 +0000

    Jun 3, 2024, 8:32 PM

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,17 @@ import Button from './Button.jsx'
 import Student from './Student.jsx'
 import UserGreeting from './UserGreeting.jsx'
 import List from './List.jsx'
+
 function App() {
-  return (
-    <>
-      <List />
-    </>
-  )
+  const fruits = [
+    { id: 1, name: 'apple', calories: 95 },
+    { id: 2, name: 'orange', calories: 45 },
+    { id: 3, name: 'banana', calories: 105 },
+    { id: 4, name: 'coconut', calories: 159 },
+    { id: 5, name: 'pineapple', calories: 37 },
+  ]
+
+  return <List items={fruits} category="Fruits" />
 }
 
 export default App
--- src/List.jsx
+++ src/List.jsx
@@ -1,22 +1,10 @@
-function List() {
-  const fruits = [
-    { id: 1, name: 'apple', calories: 95 },
-    { id: 2, name: 'orange', calories: 45 },
-    { id: 3, name: 'banana', calories: 105 },
-    { id: 4, name: 'coconut', calories: 159 },
-    { id: 5, name: 'pineapple', calories: 37 },
-  ]
-  //   fruits.sort((a, b) => a.name.localeCompare(b.name)) // ALPHABETICAL
-  //   fruits.sort((a, b) => b.name.localeCompare(a.name)) // REVERSE ALPHABETICAL
-  //   fruits.sort((a, b) => a.calories - b.calories) // NUMERIC
-  //   fruits.sort((a, b) => b.calories - a.calories) // REVERSE NUMERIC
+function List(props) {
+  const category = props.category
+  const itemList = props.items
 
-  //   const lowCalFruits = fruits.filter((fruit) => fruit.calories < 100)
-  //   const highCalFruits = fruits.filter((fruit) => fruit.calories >= 100)
-
-  const listItems = fruits.map((fruit) => (
-    <li key={fruit.id}>
-      {fruit.name}: &nbsp; <b>{fruit.calories}</b>
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp; <b>{item.calories}</b>
     </li>
   ))
   return <ol>{listItems}</ol>
