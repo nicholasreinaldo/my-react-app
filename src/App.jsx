c02a3c7cf3ba594c01c6f6fd001b332c411b83b0
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 06:04:18 +0000

    Jun 3, 2024, 1:04 PM

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
       <Student name="Spongebob" age={30} isStudent={true} />
       <Student name="Patrick" age={42} isStudent={false} />
       <Student name="Squidward" age={50} isStudent={false} />
-      <Student name="Sandy" age={27} isStudent={true} />
+      <Student name="Larry" age={27} isStudent={true} />
+      <Student />
     </>
   )
 }
