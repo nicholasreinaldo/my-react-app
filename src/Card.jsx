caaf2bf44aa0482ab5c570bb8735c6ea78724fd3
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-03 01:58:53 +0000

    Jun 3, 2024, 8:58 AM

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,11 @@
 function Card() {
-  return <div className="card"></div>
+  return (
+    <div className="card">
+      <img></img>
+      <h2>Nicholas</h2>
+      <p>I make Youtube videos and play video games</p>
+    </div>
+  )
 }
 
 export default Card
