605a192bdf3ce0d27942b8ae6e060de3942cd71f
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-04 14:57:15 +0000

    Jun 4, 2024, 9:57 PM

--- src/ClickButton.jsx
+++ src/ClickButton.jsx
@@ -1,15 +1,8 @@
 function ClickButton() {
-  let count = 0;
-  const handleClick = (name) => {
-    if(count < 3) {
-        count++;
-        console.log(`${name} you clicked me ${count} time/s`)
-    }
-    else {
-      console.log(`${name} stop clicking me!`)
-    }
-  }
-  return <button onClick={() => handleClick("Nicholas")}>Click me :D</button>
+  
+  const handleClick = (e) => console.log(e);
+    
+    return <button onClick={(e)=> handleClick(e)}>Click me :D</button>
 }
 
 export default ClickButton
