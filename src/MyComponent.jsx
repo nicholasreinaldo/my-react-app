c12428e639ff06993d561dc2d08e5c872b835f26
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-04 16:30:52 +0000

    Jun 4, 2024, 11:30 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,14 +2,23 @@ import React, {useState} from 'react';
 
 function MyComponent() {
     const [name, setName] = useState("Guest");
-    const {quantity, setQuantitiy} = useState();
-       function handleNameChange(event) {
+    const {quantity, setQuantity} = useState();
+    
+    function handleNameChange(event) {
         setName(event.target.value);
     }
+
+    function handleQuantityChange(event) {
+        setQuantity(event.target.value);
+    }
+
     return(<div>
                 <input value ={name} onChange={handleNameChange}/>
                 <p>Name: {name}</p>
-           </div>)
+
+                <input value ={quantity} onChange={handleQuantityChange} type="number"/>
+                <p>Quantity: {quantity}</p>
+            </div>)
 }
 
 export default MyComponent
