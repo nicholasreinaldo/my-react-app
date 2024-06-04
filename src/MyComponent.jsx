98257a6eb4b5d61fcac93100e124bd0fb6cc608e
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-04 15:31:35 +0000

    Jun 4, 2024, 10:31 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,6 +4,7 @@ function MyComponent() {
     
     const [name, setName] = useState("Guest");
     const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(falase);
 
     const updateName = () => {
         setName("Spongebob")
@@ -13,12 +14,19 @@ function MyComponent() {
         setAge(age + 2)
     }
 
+    const toggleEmployedStatus = () => {
+        setIsEmployed(!isEmployed);
+    }
+
     return ( <div>
                 <p>Name: {name}</p>
                 <button onClick={updateName}>Set Name</button>
 
                 <p>Age: {age}</p>
                 <button onClick={incrementAge}>Increment Age</button>
+
+                <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
+                <button onClick={toggleEmployedStatus}>Toggle Status</button>
 
              </div>);
 }
