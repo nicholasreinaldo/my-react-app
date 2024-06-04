2302fdf4a84047b0b52b12d84a5a92c37fdda4d5
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-04 15:03:18 +0000

    Jun 4, 2024, 10:03 PM

--- src/ProfilePicture.jsx
+++ src/ProfilePicture.jsx
@@ -1,6 +1,7 @@
 function ProfilePicture() {
     const imageUrl = './src/assets/profile.jpg';
-    return (<img src={imageUrl}></img>)
+    const handleClick = (e) => e.target.style.display = "none"
+    return (<img onClick = {(e) => handleClick(e)} src={imageUrl}></img>)
   }
   
   export default ProfilePicture
