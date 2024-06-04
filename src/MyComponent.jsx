f884b052cfe2d85f7621325b315c0ec6589b842a
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-04 16:29:03 +0000

    Jun 4, 2024, 11:29 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 
 function MyComponent() {
     const [name, setName] = useState("Guest");
-    function handleNameChange(event) {
+    const {quantity, setQuantitiy} = useState();
+       function handleNameChange(event) {
         setName(event.target.value);
     }
     return(<div>
