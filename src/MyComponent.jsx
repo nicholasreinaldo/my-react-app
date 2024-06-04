3c23215eba9db409f77ea65f2e7310d7432ef198
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-04 15:29:34 +0000

    Jun 4, 2024, 10:29 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,14 +3,23 @@ import React, {useState} from 'react'
 function MyComponent() {
     
     const [name, setName] = useState("Guest");
-    const [age, setAge] = useState();
+    const [age, setAge] = useState(0);
 
     const updateName = () => {
         setName("Spongebob")
     }
+
+    const incrementAge = () => {
+        setAge(age + 2)
+    }
+
     return ( <div>
                 <p>Name: {name}</p>
                 <button onClick={updateName}>Set Name</button>
+
+                <p>Age: {age}</p>
+                <button onClick={incrementAge}>Increment Age</button>
+
              </div>);
 }
 
