6db2c76aaac2a66482460000c7147de6f076fcca
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-04 15:40:14 +0000

    Jun 4, 2024, 10:40 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import ProfilePicture from './ProfilePicture'
 import MyComponent from './MyComponent'
-
+import Counter from './Counter'
 
 function App() {
-  return <MyComponent />
+  return <Counter />
+  
 }
 
 export default App
