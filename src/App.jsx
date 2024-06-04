e4fe4427cfbb0832706fd935d470fb78f901b0d7
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-04 16:28:21 +0000

    Jun 4, 2024, 11:28 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import ProfilePicture from './ProfilePicture'
 import MyComponent1 from './MyComponent (useState)'
 import Counter from './Counter'
-import MyComponent from './MyComponent (useState)'
+import MyComponent from './MyComponent'
 
 
 function App() {
