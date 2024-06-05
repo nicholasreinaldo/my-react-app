374e3063948e8c933b1194f109ecd4474afdfa6d
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-05 12:24:33 +0000

    Jun 5, 2024, 7:24 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,16 +3,24 @@ import react, { useState } from 'react'
 function MyComponent() {
   const [car, setCar] = useState({ year: 2024, make: 'Ford', model: 'Mustang' })
 
+  function handleYearChange(event) {
+    setCar({ year: 2025 })
+  }
+
+  function handleMakeChange(event) {}
+
+  function handleModelChange(event) {}
+
   return (
     <div>
       <p>
         Your favorite car is: {car.year} {car.make} {car.model}
       </p>
-      <input type="number" value={car.year} />
+      <input type="number" value={car.year} onChange={handleYearChange} />
       <br />
-      <input type="text" value={car.make} />
+      <input type="text" value={car.make} onChange={handleMakeChange} />
       <br />
-      <input type="text" value={car.model} />
+      <input type="text" value={car.model} onChange={handleModelChange} />
       <br />
     </div>
   )
