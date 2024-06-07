cfa5c1eae478d1a49cfd2c298fd3fd555e349008
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-07 13:19:15 +0000

    Jun 7, 2024, 8:19 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ProfilePicture from './ProfilePicture'
-import MyComponent1 from './MyComponent (useState)'
+import MyComponent1 from './MyComponent (useState)' // Change to import MyComponent if want to use 
 import Counter from './Counter'
-import MyComponent2 from './MyComponent (onChange)'
+import MyComponent2 from './MyComponent (onChange)' // Change to import MyComponent if want to use 
 import ColorPicker from './ColorPicker'
+import MyComponent3 from './MyComponent (object states)' // Change to import MyComponent if want to use 
 import MyComponent from './MyComponent'
 
+
 function App() {
-  return <Counter />
+  return <MyComponent />
 }
 
 export default App
