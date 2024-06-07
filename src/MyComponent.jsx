0ed04e63cac3e27264b1379b0bb69b2a7bf7f405
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-07 13:28:05 +0000

    Jun 7, 2024, 8:28 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,7 +4,7 @@ function MyComponent() {
     const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
 
     function handleAddFood(){
-
+        const newFood = document.getElementById("foodInput").ariaValueMax;
     };
 
     function handleRemoveFood(){
@@ -16,6 +16,8 @@ function MyComponent() {
         <ul>
             {foods.map((food,index) => <li key={index}>{food}</li>)}
         </ul>
+        <input type="text" id="food" placeholder="Enter food name" />
+        <button onClick={handleAddFood}></button>
     </div>);
 }
 
