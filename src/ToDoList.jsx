bf916039707ca1eb2a2238cc4a40c5d7c0dc8425
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2024-06-08 00:26:50 +0000

    Jun 8, 2024, 7:26 AM

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,52 @@
 import React, { useState } from 'react'
 
 function ToDoList() {
+    const [tasks, setTasks] = useState(["Eat Breakfast", "Take a shower", "Walk the dog"]);
+    const [newTask, setNewTask] = useState("");
     
+    function handleInputChange(event) {
+        setNewTask(event.target.value)
+    }
+
+    function addTask() {
+
+    }
+
+    function deleteTask(index) {
+
+    }
+
+    function moveTaskUp(index) {
+
+    } 
+
+    function moveTaskDown(index) {
+
+    }
+
+    return(<div className="to-do-list">
+        <h1>To-Do_list</h1>
+
+        <div>
+            <input type ="text" 
+                   placeholder="Enter a task..."
+                   value={newTask}
+                   onChange={handleInputChange}
+                   />
+
+            <button
+                className="add-button"
+                onClick={addTask}>
+                Add                    
+            </button>
+        </div>
+        <ol>
+            {tasks.map((task,index) => 
+                <li></li>
+            )}
+        </ol>
+
+    </div>)
 }
 
 export default ToDoList
