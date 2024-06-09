b0338475ca89c4e0972ebbed341641d9acbbd221
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-09 07:46:08 +0000

    Jun 9, 2024, 2:46 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import ProfilePicture from './ProfilePicture'
-import MyComponent1 from './MyComponent (useState)' // Change to import MyComponent if want to use 
+import MyComponent1 from './MyComponent (useState)' // Change to import MyComponent if want to use
 import Counter from './Counter'
-import MyComponent2 from './MyComponent (onChange)' // Change to import MyComponent if want to use 
+import MyComponent2 from './MyComponent (onChange)' // Change to import MyComponent if want to use
 import ColorPicker from './ColorPicker'
-import MyComponent3 from './MyComponent (object states)' // Change to import MyComponent if want to use 
+import MyComponent3 from './MyComponent (object states)' // Change to import MyComponent if want to use
 import MyComponent4 from './MyComponent (array states)'
 import ToDoList from './ToDoList'
+import MyComponent from './MyComponent'
 
 function App() {
-  return <ToDoList />
+  return <MyComponent />
 }
 
 export default App
