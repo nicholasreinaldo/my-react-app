ba22c4354ce1e96470de1aad77d9414aac4b96d2
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-09 07:52:40 +0000

    Jun 9, 2024, 2:52 PM

---
+++ src/MyComponent.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+
+function MyComponent() {
+  const [count, setCount] = useState(0)
+
+  function addCount() {
+    setCount((c) => c + 1)
+  }
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={addCount}>Add</button>
+    </>
+  )
+}
+
+export default MyComponent
