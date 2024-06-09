94d557b144ca56b9a774c064f101264d8ca46999
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-09 11:38:00 +0000

    Jun 9, 2024, 6:38 PM

--- src/ComponentA.jsx
+++ src/ComponentA.jsx
@@ -1,7 +1,15 @@
+import React, { useState } from 'react'
+
+import ComponentB from './ComponentB'
+
 function ComponentA() {
+  const [user, setUser] = useState('Nicho')
+
   return (
     <div className="box">
       <h1>ComponentA</h1>
+      <h2>{`Hello ${user}`}</h2>
+      <ComponentB user={user} />
     </div>
   )
 }
---
+++ src/ComponentD.jsx
@@ -0,0 +1,10 @@
+function ComponentD() {
+  return (
+    <div className="box">
+      <h1>ComponentD</h1>
+      <h2>{`Bye ${props.user}`}</h2>
+    </div>
+  )
+}
+
+export default ComponentD
