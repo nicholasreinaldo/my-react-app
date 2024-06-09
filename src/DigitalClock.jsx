627d869bc1ba50921035a068d803fd2e752c3485
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-09 08:47:50 +0000

    Jun 9, 2024, 3:47 PM

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react'
 
 function DigitalClock() {
-  return <></>
+  return (
+    <div className="clock-container">
+      <div className="clock">
+        <span>00:00:00</span>
+      </div>
+    </div>
+  )
 }
 
 export default DigitalClock
