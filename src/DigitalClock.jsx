07254c5d18057f73f9de73502d00ca704847e12e
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-09 08:56:09 +0000

    Jun 9, 2024, 3:56 PM

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react'
 
 function DigitalClock() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
   return (
     <div className="clock-container">
       <div className="clock">
