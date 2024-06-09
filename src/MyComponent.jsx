20b93b3966170d1ed6e2dc52a8707afe04650274
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-09 08:33:41 +0000

    Jun 9, 2024, 3:33 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,7 +3,25 @@
 import React, { useState, useEffect } from 'react'
 
 function MyComponent() {
-  return <></>
+  const [width, setWidth] = useState(window.innerWidth)
+  const [height, setHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    console.log('EVENT LISTENER ADDED')
+  }, [])
+
+  function handleResize() {
+    setWidth(window.innerWidth)
+    setHeight(window.innerHeight)
+  }
+
+  return (
+    <>
+      <p>Window Width: {width} px</p>
+      <p>Window Heihgt: {height} px</p>
+    </>
+  )
 }
 
 export default MyComponent
