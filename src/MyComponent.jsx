bc249acdcdb45cbdeaf4bf4a6f8cbbc28b78c69a
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-09 08:22:45 +0000

    Jun 9, 2024, 3:22 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,19 +2,30 @@ import React, { useState, useEffect } from 'react'
 
 function MyComponent() {
   const [count, setCount] = useState(0)
+  count[(color, setColor)] = useState('Green')
 
   useEffect(() => {
     document.title = `Count: ${count}`
-  }, [])
+  }, [count])
 
   function addCount() {
     setCount((c) => c + 1)
+  }
+
+  function substractCount() {
+    setCount((c) => c - 1)
+  }
+
+  function changeColor() {
+    setColor((c) => (c === 'green' ? 'red' : 'green'))
   }
 
   return (
     <>
       <p>Count: {count}</p>
       <button onClick={addCount}>Add</button>
+      <button onClick={substractCount}>Substract</button>
+      <button onClick={changeColor}>Change Color</button>
     </>
   )
 }
