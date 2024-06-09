f6a483f35a8c2cdc25abc40a64b0d2e4e58edc23
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-09 11:49:34 +0000

    Jun 9, 2024, 6:49 PM

--- src/ComponentA.jsx
+++ src/ComponentA.jsx
@@ -2,6 +2,8 @@ import React, { useState, createContext } from 'react'
 
 import ComponentB from './ComponentB'
 
+export const UserContext = createContext()
+
 function ComponentA() {
   const [user, setUser] = useState('Nicho')
 
@@ -9,7 +11,9 @@ function ComponentA() {
     <div className="box">
       <h1>ComponentA</h1>
       <h2>{`Hello ${user}`}</h2>
-      <ComponentB user={user} />
+      <UserContext.Provider value={user}>
+        <ComponentB user={user} />
+      </UserContext.Provider>
     </div>
   )
 }
