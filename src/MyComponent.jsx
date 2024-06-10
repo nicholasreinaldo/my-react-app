a97a3b7b8a767a2b5f870aa535c5b1719bc0cacf
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-10 11:21:05 +0000

    Jun 10, 2024, 6:21 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,25 +1,37 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 function MyComponent() {
-  const inputRef = useRef(null)
+  const inputRef1 = useRef(null)
+  const inputRef2 = useRef(null)
+  const inputRef3 = useRef(null)
 
   useEffect(() => {
     console.log('COMPONENT RENDERED')
   })
 
-  function handleClick() {
-    inputRef.current.focus()
-    inputRef.current.style.background = 'yellow'
+  function handleClick1() {
+    inputRef1.current.focus()
+    inputRef1.current.style.background = 'yellow'
+  }
+
+  function handleClick2() {
+    inputRef2.current.focus()
+    inputRef2.current.style.background = 'yellow'
+  }
+
+  function handleClick3() {
+    inputRef3.current.focus()
+    inputRef3.current.style.background = 'yellow'
   }
 
   return (
     <div>
-      <button onClick={handleClick}>Click me!</button>
-      <input ref={inputRef} />
-      <button onClick={handleClick}>Click me!</button>
-      <input ref={inputRef} />
-      <button onClick={handleClick}>Click me!</button>
-      <input ref={inputRef} />
+      <button onClick={handleClick1}>Click me 1!</button>
+      <input ref={inputRef1} />
+      <button onClick={handleClick2}>Click me 2!</button>
+      <input ref={inputRef2} />
+      <button onClick={handleClick3}>Click me 3!</button>
+      <input ref={inputRef3} />
     </div>
   )
 }
