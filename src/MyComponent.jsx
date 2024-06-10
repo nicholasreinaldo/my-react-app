d697edd18003dcfc871f7d1fcba6b25768b0e1f1
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-10 11:16:31 +0000

    Jun 10, 2024, 6:16 PM

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,18 +1,25 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 function MyComponent() {
-  const ref = useRef(0)
+  const ref = useRef(null)
+
+  console.log(ref.current)
 
   useEffect(() => {
     console.log('COMPONENT RENDERED')
+    console.log(inputRef)
   })
 
   function handleClick() {
     ref.current++
-    console.log(ref.current)
   }
 
-  return <button onClick={handleClick}>Click me!</button>
+  return (
+    <div>
+      <button onClick={handleClick}>Click me!</button>
+      <input />
+    </div>
+  )
 }
 
 export default MyComponent
