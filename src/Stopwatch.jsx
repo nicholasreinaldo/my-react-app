4f93454c9256de2225db0d5b086a0e0ab3ddc948
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-10 11:27:02 +0000

    Jun 10, 2024, 6:27 PM

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-function Stopwatch() {}
+function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false)
+  const [elapsedTime, setElapsedTime] = useState(0)
+  const intervalIdRef = useRef(null)
+  const startTimeRef = useRef(0)
+
+  return <></>
+}
 
 export default Stopwatch
