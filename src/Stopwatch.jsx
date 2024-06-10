255158dec3cddceeec33b870fddd50ec275aa033
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-10 11:44:14 +0000

    Jun 10, 2024, 6:44 PM

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -33,7 +33,17 @@ function Stopwatch() {
   }
 
   function formatTime() {
-    return `00:00:00`
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
+    let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60)
+    let seconds = Math.floor((elapsedTime / 1000) % 60)
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10)
+
+    hours = String(hours).padStart(2, '0')
+    minutes = String(minutes).padStart(2, '0')
+    seconds = String(seconds).padStart(2, '0')
+    milliseconds = String(milliseconds).padStart(2, '0')
+
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`
   }
 
   return (
